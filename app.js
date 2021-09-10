bbfd8fe37939140ff22aa5634205a9856c98063f
jeigabi <dajeik427@gmail.com>
2021-09-10 21:59:23 +0900

    login 구현

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const loginForm = document.querySelector("#login-form");
-const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
+const loginInput = loginForm.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
-function onLoginBtnClick() {
-    console.log("Hello", loginInput.value);
+function onLoginSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+
+    const username = loginInput.value; 
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
+}
+
+function paintGreetings(username) {
+    greeting.innerHTML = `Hello ${username}`;    
+    greeting.classList.remove(HIDDEN_CLASSNAME);    
 }
 
 
-loginButton.addEventListener("click", onLoginBtnClick);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
